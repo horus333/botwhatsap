2cbbd145a915c169a078826c43db197e5f2286ad
DavidBurbano <tantanyringring@gmail.com>
2021-03-27 20:25:38 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# ShanBot
+Pasos para instalar el bot via termux
+https://youtu.be/rfxuYsWGmQo
+
+pkg install git
+
+pkg install ffmpeg
+
+pkg install wget
+
+pkg install nodejs
+
+pkg install npm
+
+git clone https://github.com/shanduy/ShanBot
+
+cd ShanBot
+
+bash install.sh
+
+npm start
+
+Escaneas el QR y listo comando para activarlo  *help
---
+++ src/help.js
@@ -0,0 +1,64 @@
+const help = (prefix) => {
+	return `
+「 *Shan Bot* 」
+
+◪ *Información*
+  ❏ Comando: 「  ${prefix}  」
+  ❏ Creador: H3r3s
+  ❏ Sigueme en mi canal :v: https://www.youtube.com/channel/UCyCX_8fUr8c-sh2yCKALEBA
+
+
+◪ *Para usar al bot*
+  ❏ Registrate con el comando ${prefix}daftar Nombre
+
+◪ *SOBRE*
+  │
+  ├─ ❏ ${prefix}info
+  ├─ ❏ ${prefix}blocklist
+  ├─ ❏ ${prefix}chatlist
+  ├─ ❏ ${prefix}ping
+  └─ ❏ ${prefix}bugreport
+
+◪ *CREAR*
+  │
+  ├─ ❏ ${prefix}sticker
+  ├─ ❏ ${prefix}stickergif
+  ├─ ❏ ${prefix}toimg
+  └─ ❏ ${prefix}tomp3 
+
+◪ *DESCARGAS*
+  │
+  └─ ❏ ${prefix}ytmp4
+
+◪ *AUDIO*
+  │
+  ├─ ❏ ${prefix}play
+  ├─ ❏ ${prefix}bahasa
+  └─ ❏ ${prefix}tts
+
+◪ *DUEÑO*
+  │
+  ├─ ❏ ${prefix}setprefix
+  ├─ ❏ ${prefix}block
+  ├─ ❏ ${prefix}bc
+  ├─ ❏ ${prefix}bcgc
+  ├─ ❏ ${prefix}clone
+  └─ ❏ ${prefix}clearall
+
+◪ *OTROS*
+  │
+  ├─ ❏ ${prefix}send
+  ├─ ❏ ${prefix}wame
+  ├─ ❏ ${prefix}virtex (Hernqn)
+  ├─ ❏ ${prefix}exe
+  ├─ ❏ ${prefix}qrcode
+  ├─ ❏ ${prefix}afk
+  ├─ ❏ ${prefix}timer
+  ├─ ❏ ${prefix}fml
+  └─ ❏ ${prefix}fml2
+
+chupalo ctm .i.
+`
+}
+
+exports.help = help
